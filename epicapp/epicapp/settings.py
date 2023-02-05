024beed71f3564738650adbde4c42d3831814df4
Neilzon <neilcviloria@gmail.com>
2023-02-04 22:30:06 -0700

    initial setup for django rest

--- epicapp/epicapp/settings.py
+++ epicapp/epicapp/settings.py
@@ -35,6 +35,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'epicappAPI',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
