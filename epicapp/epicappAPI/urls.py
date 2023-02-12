f878e828c60c69f50e00205916d0a0093bef2bc9
Neilzon <neilcviloria@gmail.com>
2023-02-11 22:07:51 -0700

    create post endpoint

--- epicapp/epicappAPI/urls.py
+++ epicapp/epicappAPI/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('signin/', views.signin, name='signin'),
     path('author/<str:id>', views.get_author, name='get_author'),
     path('authors/', views.get_authors, name='get_authors'),
+
+    # post paths
+    path('authors/<str:author_id>/posts/', views.create_post, name='create_post')
 ]
