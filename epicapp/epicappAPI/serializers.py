cf4b8990032f4706c3a30cfef76e5aace6548b2d
Shane Goonasekera <theshanukg@gmail.com>
2023-03-17 22:10:57 -0600

    better image api

--- epicapp/epicappAPI/serializers.py
+++ epicapp/epicappAPI/serializers.py
@@ -21,7 +21,8 @@ class AuthorSerializer(serializers.ModelSerializer):
         validated_data['id'] = id
         validated_data['url'] = f"{HOST}/authors/{id}"
         validated_data['host'] = f"{HOST}/"
-        validated_data['profile_image'] = f"https://api.multiavatar.com/{id}.svg"
+        validated_data[
+            'profile_image'] = f"https://api.dicebear.com/5.x/micah/svg?backgroundColor=fffd01&seed={id}"
         # hash password
         validated_data['password'] = make_password(validated_data['password'])
         return Author.objects.create(**validated_data)
