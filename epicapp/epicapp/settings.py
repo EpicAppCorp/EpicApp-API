fcb6f42b5fb640fd8f25e709ca6e19cdb6d1d2de
Shane Goonasekera <theshanukg@gmail.com>
2023-03-25 17:03:29 -0600

    just to test

--- epicapp/epicapp/settings.py
+++ epicapp/epicapp/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = 'django-insecure-v=g&ya+!wr+f*vvt7awvvs^7^#mc0+)v7v4l7h$rs0x^^dl_i)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False if env(
-    "ENV") == "PROD" else True
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '*'
