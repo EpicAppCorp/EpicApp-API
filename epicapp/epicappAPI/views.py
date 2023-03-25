6505324c607718f87314ffdf9aad0c78fa80f9be
Shane Goonasekera <theshanukg@gmail.com>
2023-03-25 15:33:23 -0600

    COOKIES AHH

--- epicapp/epicappAPI/views.py
+++ epicapp/epicappAPI/views.py
@@ -104,7 +104,7 @@ class AuthenticateView(APIView):
                             status=status.HTTP_200_OK)
 
         response.set_cookie(key='access', value=token,
-                            secure=True, httponly=True, samesite='Strict')
+                            secure=True, httponly=True, samesite='None')
 
         # this one is for dev
         # response.set_cookie(key='access', value=token,
