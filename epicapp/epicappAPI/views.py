e70127c29d63fbcb425a08a47225b1dfab47c893
Shane Goonasekera <theshanukg@gmail.com>
2023-03-25 02:21:28 -0600

    fixed inbox comments and likes

--- epicapp/epicappAPI/views.py
+++ epicapp/epicappAPI/views.py
@@ -472,7 +472,7 @@ class CommentsView(APIView):
     def post(self, request, author_id, post_id):
         comment_data = request.data
         comment_data["post_id"] = post_id
-        comment_data["author_id"] = author_id
+        comment_data["author_id"] = comment_data["author"]['id'].split('/')[-1]
         comment = CommentSerializer(data=comment_data)
 
         if not comment.is_valid():
@@ -555,19 +555,12 @@ class InboxView(APIView):
 
                 like = Like.objects.get(id=inbox_item.object_id)
                 formatted_like = LikeSerializer(like).data
-                author = ''
-
-                # if url is from us, just get from models and not make another request to same server
-                if (HOST in like.author):
-                    author = AuthorSerializer(Author.objects.filter(
-                        id=like.author.split('/')[-1]).first()).data
-                else:
-                    author = requests.get(like.author).json()
 
                 # format stuff
                 del formatted_like['id']  # not needed in final representation
-                formatted_like['author'] = author
-                formatted_like['summary'] = f"{author['displayName']} likes your post"
+                formatted_like['summary'] = f"{formatted_like['author']['displayName']} likes your post"
+
+                print(formatted_like)
 
                 data.append(formatted_like)
 
@@ -600,6 +593,7 @@ class InboxView(APIView):
                 else:
                     author = requests.get(formatted_comment['author']).json()
 
+                formatted_comment["author"] = author
                 data.append(formatted_comment)
 
         data = {
@@ -627,7 +621,6 @@ class InboxView(APIView):
         type = data["type"]
 
         if type.upper() == "LIKE":
-            data['author_id'] = id
             url_components = data['object'].split('/')
             object_id = url_components[-1]
 
